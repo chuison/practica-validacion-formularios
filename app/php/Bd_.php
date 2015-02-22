f5ebfb466737ecda5266ac9c6876b550c569fe6d
chuison <chuison.alvarez@gmail.com>
2015-02-22 20:32:02 +0100

    formulario sin acceso a bases de datos

---
+++ app/php/Bd_.php
@@ -0,0 +1,23 @@
+<?php
+//Renombrar a Bd.php y completar con los datos de la conexión
+
+class Bd {
+
+	//local
+	public static $dbCp = "luisalvarez_1";
+	public static $dbCli = "luisalvarez_1";
+	public static $user = "luisalvarez_1";
+	public static $pass = "luisalvarez";
+	public static $host = "localhost";
+
+	//servidor
+	/*
+	public static $dbCp = "";	//BD de codigos postales y municipios
+	public static $dbCli = "";	//BD de clientes (para verificar nif, cif, usuario, email, iban)
+	public static $user = "";	//Usuario de mysql
+	public static $pass = "";	//Password de mysql
+	public static $host = "";	//Ruta de la máquina mysql
+	*/
+}
+
+?>
